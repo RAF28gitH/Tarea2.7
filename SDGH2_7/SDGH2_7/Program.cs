3dc61a834310b77a9d373abb4d42bc290bad4e79
RAF28gitH <raf28elnooob@gmail.com>
2024-10-22 07:47:34 -0600

    archivo base creado, aún sin nada

---
+++ SDGH2_7/SDGH2_7/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC16
+ * Date: 22/10/2024
+ * Time: 07:44 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace SDGH2_7
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
